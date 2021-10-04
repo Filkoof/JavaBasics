96e10bbb9a8f662d1b2b544ff49ff6f200a14926
artem_filkov <filkof@mail.ru>
2021-10-04 20:30:47 +0300

    Задание 4.2 вторая часть.

---
+++ 04_Encapsulation/02_Pojo/Phone.java
@@ -0,0 +1,47 @@
+public class Phone {
+
+    private String nameBrand;
+    private int memory;
+    private String nameModel;
+    private boolean haveCamera;
+
+    public Phone(String nameBrand, String nameModel) {
+        this.nameBrand = nameBrand;
+        this.nameModel = nameModel;
+    }
+
+    public String getNameBrand() {
+        return nameBrand;
+    }
+
+    public void setNameBrand(String nameBrand) {
+        this.nameBrand = nameBrand;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public String getNameModel() {
+        return nameModel;
+    }
+
+    public void setNameModel(String nameModel) {
+        this.nameModel = nameModel;
+    }
+
+    public boolean setHaveCamera() {
+        return haveCamera;
+    }
+
+    public void getHaveCamera(boolean haveCamera) {
+        this.haveCamera = haveCamera;
+    }
+
+
+
+}
