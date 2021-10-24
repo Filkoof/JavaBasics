bf50ccc03c4536dfeb824606b4fc7f45a1aac749
artem_filkov <filkof@mail.ru>
2021-10-24 20:47:54 +0300

    Задание 4.10 процесс

---
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Delivery.java
@@ -0,0 +1,68 @@
+package ru.skillbox;
+
+public class Delivery {
+    private final int dimensions;
+    private final int weight;
+    private final String address;
+    private final String canFlip;
+    private final String registrationNumber;
+    private final String delicate;
+
+    public Delivery(int dimensions,
+                    int weight,
+                    String address,
+                    String canFlip,
+                    String registrationNumber,
+                    String delicate) {
+        this.dimensions = dimensions;
+        this.weight = weight;
+        this.address = address;
+        this.canFlip = canFlip;
+        this.registrationNumber = registrationNumber;
+        this.delicate = delicate;
+    }
+
+    public int getDimensions() {
+        return dimensions;
+    }
+
+    public Delivery setDimensions(int dimensions) {
+        return new Delivery(dimensions, weight, address , canFlip, registrationNumber, delicate);
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public Delivery setWeight(int weight) {
+        return new Delivery(dimensions, weight, address , canFlip, registrationNumber, delicate);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Delivery setAddress(String address) {
+        return new Delivery(dimensions, weight , address , canFlip, registrationNumber, delicate);
+    }
+
+    public String getCanFlip() {
+        return canFlip;
+    }
+
+    public Delivery setCanFlip(String canFlip) {
+        return new Delivery(dimensions, weight, address, canFlip, registrationNumber, delicate);
+    }
+
+    public String getDelicate() {
+        return delicate;
+    }
+
+    public Delivery setDelicate(String delicate) {
+        return new Delivery(dimensions, weight, address, canFlip, registrationNumber, delicate);
+    }
+
+
+
+
+}
--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -3,5 +3,16 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions();
+        dimensions.setWidth();
+        dimensions.setHeight();
+        dimensions.setLengt();
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress();
+        delivery.setWeight();
+
+        Dimensions copy = new Dimensions(dimensions.getWidth(), dimensions.getHeight(), dimensions.getLength());
+
     }
 }
