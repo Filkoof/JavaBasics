27eb600b5dcec96da73f96029c8d0f073c010bef
artem_filkov <filkof@mail.ru>
2021-11-02 19:54:02 +0300

    Задание 4.10 процесс

--- 05_StaticFinal/01_Static/Main.java
+++ 05_StaticFinal/01_Static/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Basket basket = new Basket("Товары", 556);
+        Basket basket = new Basket("Колбаса", 556);
+        Basket basket1 = new Basket("Тарелка", 130);
+        Basket basket2 = new Basket("Весло", 880);
         System.out.println(Basket.getCount());
-
     }
 }
