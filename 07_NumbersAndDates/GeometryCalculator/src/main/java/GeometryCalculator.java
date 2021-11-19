7471d41f8e32107b0a04357cf4e8f2b58c21821c
artem_filkov <filkof@mail.ru>
2021-11-19 18:12:41 +0300

    7.7 задание 1.

--- 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,42 @@
+package main.java;
+
 public class GeometryCalculator {
+    private static double circleSquare;
+    private static double sphereVolume;
+    private static double triangleSquare;
+
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+        Math.abs(radius);
+        circleSquare = Math.PI * Math.pow(radius, 2);
+        return circleSquare ;
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+        Math.abs(radius);
+        sphereVolume = 4/3 * Math.PI * Math.pow(radius, 3);
+        return sphereVolume;
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
+        if (a + b > c || b + c > a || c + a > b) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+        double p = (a + b + c) / 2;
+        if(isTrianglePossible(a,b,c)) {
+            triangleSquare = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+            return triangleSquare;
+        } else {
+            System.out.println("Triangle is not possible ");
+            return -1.0;
+        }
     }
 }
