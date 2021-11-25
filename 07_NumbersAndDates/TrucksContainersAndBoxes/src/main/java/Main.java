c76fbfe44d4897fdcad2dffe7d1ea1b2e31c97b3
artem_filkov <filkof@mail.ru>
2021-11-25 22:47:08 +0300

    Тесты успешны.

--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,11 +1,9 @@
-package main.java;
-
 import java.util.Scanner;
 
 public class Main {
 
-    private static int CAPACITY_TRUCK = 12; //Количество контейнеров вмещающееся в грузовик
-    private static int CAPACITY_CONTAINER = 27; //Количество ящиков вмещающееся в контейнер
+    private static int CAPACITY_TRUCK = 12;
+    private static int CAPACITY_CONTAINER = 27;
 
     public static void main(String[] args) {
 
@@ -16,26 +14,25 @@ public class Main {
         int container = 0;
         int box = Integer.parseInt(boxes);
 
-        if (box > 0){
-            ++truck;
-            ++container;
-            System.out.println("Грузовик: " + truck);
-            System.out.println("\tКонтейнер: " + container);
-        }
+        for (int i = 0; i < box; i++) {
 
-        for (int i = 0; i <= box; i++) {
-            System.out.println("\t\tЯщик: " + (i + 1));
-            if (i % (CAPACITY_TRUCK * CAPACITY_CONTAINER) == 0) {
-                ++truck;
-                System.out.println("Грузовик " + truck);
+            if((i % (CAPACITY_TRUCK * CAPACITY_CONTAINER) == 0) && box != 0){
+                truck++;
+                System.out.println("Грузовик: " + truck);
+
             }
-            if (i % CAPACITY_CONTAINER == 0) {
-                ++container;
+
+            if((i % CAPACITY_CONTAINER == 0) && box != 0) {
+                container++;
                 System.out.println("\tКонтейнер: " + container);
             }
+            System.out.println("\t\tЯщик: " + (i + 1));
+
         }
-        System.out.println("Необходимо:\tгрузовиков - " + truck + "шт.");
-        System.out.println("Контейнеров - " + container + "шт.");
+
+        System.out.println("Необходимо:");
+        System.out.println("грузовиков - " + truck + " шт.");
+        System.out.println("контейнеров - " + container + " шт.");
 
     }
 
