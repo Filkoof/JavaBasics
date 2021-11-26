66c64c3eb0c9b15e1c2b700f41b8459b1f4f0b31
artem_filkov <filkof@mail.ru>
2021-11-26 17:20:13 +0300

    Вывод правильный,тест не проходит

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,22 +1,35 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int day = 31;
-        int month = 12;
-        int year = 1990;
+        int day = 7;
+        int month = 4;
+        int year = 1996;
 
         System.out.println(collectBirthdays(year, month, day));
 
     }
 
     public static String collectBirthdays(int year, int month, int day) {
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - E")
+                .localizedBy(new Locale("us"));
 
-        //TODO реализуйте метод для построения строки в следующем виде
-        //0 - 31.12.1990 - Mon
-        //1 - 31.12.1991 - Tue
-        
-        return "";
+        for (int i = 0;i >= 0 ; i++){
+            if (birthday.isBefore(today) || birthday.equals(today)){
+                System.out.println(i + " - " + formatter.format(birthday));
+                birthday = birthday.plusYears(1);
+            }
+            else {
+                break;
+            }
+        }
+
+        return " ";
     }
 }
