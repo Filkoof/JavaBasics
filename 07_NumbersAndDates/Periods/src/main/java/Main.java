f56766f4fbd581dccbe556b8eb67c8e261f4cf9e
artem_filkov <filkof@mail.ru>
2021-11-27 21:14:46 +0300

    8.10 Задание 2.

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,26 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 public class Main {
 
   public static void main(String[] args) {
 
+    LocalDate birthday = LocalDate.of(1996,01,21);
+
+    System.out.println();
+
   }
 
   private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
+    LocalDate now = LocalDate.now();
+    long difference = now.toEpochDay() - birthday.toEpochDay();
+    int year = (int) (difference / 365);
+    int month = (int) (difference % 365) / 31;
+    int day = (int) (difference % 365) % 31;
+
+    return " ";
   }
 
 }
