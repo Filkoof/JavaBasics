3080b2400a7a7cba3bce1a0afadc36b72b0eb240
artem_filkov <filkof@mail.ru>
2021-11-30 17:48:47 +0300

    9.9 Задание 1. Готово

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -5,17 +5,18 @@ public class Main {
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
     String numbers = "";
-    String sum = "";
+    int sum = 0;
 
     for (int i = 0; i < text.length(); i++) {
       char symbol = text.charAt(i);
       if(Character.isDigit(symbol)){
-        numbers = numbers + symbol;
+        numbers += symbol;
       }
-      else if(symbol == ' '){
-        sum = sum + numbers;
-        numbers = " ";
+      else if(!numbers.equals("")) {
+        sum += Integer.parseInt(numbers);
+        numbers = "";
       }
+
     }
 
     System.out.println(sum);
