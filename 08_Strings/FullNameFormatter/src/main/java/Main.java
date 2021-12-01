f3b55367ea2c1e39859eda5d1f6c3b5b9047391b
artem_filkov <filkof@mail.ru>
2021-12-01 22:18:24 +0300

    Задание 2. Готово

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -5,14 +5,39 @@ public class Main {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
+
+    String surname = "";
+    String name = "";
+    String lastName  = "";
+    int count = 0;
+    String error = "";
+
     while (true) {
       String input = scanner.nextLine();
-      if (input.equals("0")) {
+      for (int i = 0; i < input.length(); i++){
+        char symbol = input.charAt(i);
+        if (symbol == ' '){
+          count++;
+          continue;
+        }
+        if(Character.UnicodeBlock.of(symbol).equals(Character.UnicodeBlock.CYRILLIC) || symbol == '-'){
+          if(count == 0) surname += symbol;
+          if(count == 1) name += symbol;
+          if(count == 2) lastName += symbol;
+        } else {
+          error = "Введенная строка не является ФИО";
+          break;
+        }
+      }
+      if (error != "" || count < 2) {
+        System.out.println("Введенная строка не является ФИО");
+        break;
+      } else {
+        System.out.println("Фамилия: " + surname);
+        System.out.println("Имя: " + name);
+        System.out.println("Отчество: " + lastName);
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
     }
   }
-
 }
