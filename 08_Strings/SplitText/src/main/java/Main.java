f4dbe69188cbaae4b389c512f29b52287cd1f939
artem_filkov <filkof@mail.ru>
2021-12-03 23:14:35 +0300

    Задание 4. Процесс

--- 08_Strings/SplitText/src/main/java/Main.java
+++ 08_Strings/SplitText/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -5,8 +8,12 @@ public class Main {
   }
 
   public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
+    String[] words = text.split("^[a-z]");
+    for(int i = 0; i < words.length; i++){
+      System.out.println(words[i]);
+    }
+
+    return words;
   }
 
 }
