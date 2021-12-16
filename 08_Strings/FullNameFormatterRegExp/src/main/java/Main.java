b94c14778b9e8b441c2bf1f36cf5e19f1c0bcf93
artem_filkov <filkof@mail.ru>
2021-12-16 15:29:04 +0300

    Revert "Задание 5. процес"
    This reverts commit 1e574b5e

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -6,30 +6,24 @@ public class Main {
 
   public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    String input = scanner.nextLine();
+    //Scanner scanner = new Scanner(System.in);
+   // String input = scanner.nextLine();
 
     String fio = "Салтыков-Щедрин Михаил Евграфович";
-    String surname, name, lastName;
+    String space = "[\\s]";
     String regex = "[А-Яа-я\\-]+";
 
     Pattern pattern = Pattern.compile(regex);
     Matcher matcher = pattern.matcher(fio);
 
-    int firstIndex = input.indexOf(" ");
-    int lastIndexOf = input.lastIndexOf(" ");
+    int count = 0;
 
-    if (matcher.find()) {
-      surname = input.substring(0,firstIndex);
-      name = input.substring(firstIndex, lastIndexOf);
-      lastName = input.substring(lastIndexOf);
-      System.out.println("Фамилия: " + surname);
-      System.out.println("Имя: " + name);
-      System.out.println("Отчество: " + lastName);
-    } else {
-      System.out.println("Введенная строка не является ФИО.");
+    while (matcher.find()) {
+      int start = matcher.start();
+      int end = matcher.end();
+      System.out.println(fio.substring(start, end));
+      }
     }
 
-  }
-
 }
+
