95fa62f6be018556404e3ad59aa05422175b52bf
artem_filkov <filkof@mail.ru>
2022-01-18 14:15:56 +0300

    Задание 10.5 процесс.

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -1,26 +1,48 @@
 public class Hospital {
 
+    private static final float MIN_TEMPERATURE = 36.2f;
+    private static final float MAX_TEMPERATURE = 36.9f;
+
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
         float[] patientTemperature = new float[patientsCount];
 
         for(int i = 0; i < patientsCount; i++){
-            patientTemperature[i] = Math.round((float) (Math.random() * ((40 - 32) + 1)) + 32);
+            float value = ((float) (Math.random() * ((40 - 32) + 1)) + 32);
+            patientTemperature[i] = value;
         }
 
         return patientTemperature;
     }
 
     public static String getReport(float[] temperatureData) {
-        /*
-        TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
-            а также температуры всех пациентов.
-        */
+
+        float sumTemperature = 0;
+        float temperatures;
+        int healthCount = 0;
+        String temperaturesPatients = "";
+
+        for(int i = 0; i< temperatureData.length; i++) {
+            sumTemperature += temperatureData[i];
+            temperaturesPatients = temperaturesPatients.concat
+                    (String.valueOf((float) (Math.round((temperatureData[i]) *
+                    Math.pow(10, 1)) / Math.pow(10, 1)))).concat(" ");
+            temperatures = (float) (Math.round((temperatureData[i]) * Math.pow(10, 1)) / Math.pow(10, 1));
+            if(temperatures >= MIN_TEMPERATURE && temperatures <= MAX_TEMPERATURE) {
+                healthCount++;
+            }
+        }
+
+        float averageTemperature = (float) (Math.round((sumTemperature / temperatureData.length)
+                * Math.pow(10, 2)) / Math.pow(10, 2));
+
+        String averageTemperaturePatients = Float.toString(averageTemperature);
+        String healthCountPatients = Integer.toString(healthCount);
 
         String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+                "Температуры пациентов: " + temperaturesPatients.trim()  +
+                        "\nСредняя температура: " + averageTemperaturePatients +
+                        "\nКоличество здоровых: " + healthCountPatients;
 
         return report;
     }
