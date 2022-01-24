a0be68f8df2d11ed210b0467cd207b43cc822268
artem_filkov <filkof@mail.ru>
2022-01-24 12:56:45 +0300

    Задание 10.9 тесты прошел.

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -1,20 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.TreeSet;
 
 public class EmailList {
 
-    public TreeSet<String> emailList = new TreeSet<>();
+    public TreeSet<String> emailSet = new TreeSet<>();
+    public ArrayList<String> emailList = new ArrayList<>();
 
     public void add(String email) {
+        email = email.toLowerCase(Locale.ROOT);
         if (checkEmailStyle(email)){
-            emailList.add(email);
+            emailSet.add(email);
         } else {
             System.out.println("Неверный формат email");
         }
     }
 
-    public TreeSet<String> getSortedEmails() {
-        for (String emails: emailList){
-            System.out.println(emails);
+    public ArrayList<String> getSortedEmails() {
+        for (String emails: emailSet){
+            String[] temp = emails.split(",");
+            emailList.addAll(Arrays.asList(temp));
         }
         return emailList;
     }
