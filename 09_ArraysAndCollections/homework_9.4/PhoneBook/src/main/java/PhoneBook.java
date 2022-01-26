f7ed3c647a2cdf6589c572c67531512852857f7e
artem_filkov <filkof@mail.ru>
2022-01-26 11:49:44 +0300

    Задание 10.11 процесс

--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
@@ -9,25 +9,23 @@ public class PhoneBook {
 
     public void addContact(String phone, String name) {
 
-        if (checkName(name) && checkPhone(phone)){
+        if (checkName(name) && checkPhone(phone)) {
             if (phoneBook.containsValue(phone)) {
                 phoneBook.put(phone, phoneBook.get(name));
             } else {
                 phoneBook.put(phone, name);
             }
         }
-        // проверьте корректность формата имени и телефона (отдельные методы для проверки)
-        // если такой номер уже есть в списке, то перезаписать имя абонента
     }
 
     public String getContactByPhone(String phone) {
-
+        String contacts = "";
         if (phoneBook.containsValue(phone)){
             System.out.println("Надо вывести связку ключ + значение");
         }
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку\
-        return "";
+        return contacts;
     }
 
     public Set<String> getContactByName(String name) {
@@ -44,23 +42,12 @@ public class PhoneBook {
     public Set<String> getAllContacts() {
         TreeSet <String> contacts = new TreeSet<>();
 
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()){
-            String phone = entry.getKey();
-            String name = entry.getValue();
-            for (Map.Entry<String, String> entry1 : phoneBook.entrySet()){
-                String phone1 = entry1.getKey();
-                String name1 = entry1.getValue();
-
-                String contact;
-                if (name == name1){
-                    contact = name + " - " + phone + " - " + phone1;
-                } else {
-                    contact = name + " - " + phone;
-                }
-                contacts.add(contact);
+        if (!phoneBook.isEmpty()) {
+            for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+                contacts.add(entry.getValue() + " - " + entry.getKey());
+                System.out.println(contacts);
             }
         }
-
         return contacts;
     }
 
