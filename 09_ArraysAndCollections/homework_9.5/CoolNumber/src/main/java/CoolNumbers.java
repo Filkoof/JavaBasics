a0568545ffd53d8fde8026572f525d28e5d3cb8c
artem_filkov <filkof@mail.ru>
2022-01-28 17:39:38 +0300

    10.13 начало

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,28 +1,55 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        ArrayList <String> numbers = new ArrayList<>();
+        String[] letters = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
+
+        for (String x : letters){
+            for (String y : letters) {
+                for (String z : letters) {
+                    for (int n = 0; n <= 9; n++) {
+                        for (int r = 0; r <= 199; r++) {
+                            numbers.add(x + n + n + n + y + z + r);
+                        }
+                    }
+                }
+            }
+        }
+
+        return numbers;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
+
+        for (int i = 0; i < list.size(); i++){
+            if (number.equals(list.get(i))){
+                return true;
+            }
+        }
         return false;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
+        if (sortedList.contains(Arrays.binarySearch(new List[]{sortedList}, number))) {
+            return true;
+        }
         return false;
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
+        if (hashSet.contains(number)){
+            return true;
+        }
         return false;
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
+        if (treeSet.contains(number)){
+            return true;
+        }
         return false;
     }
 
