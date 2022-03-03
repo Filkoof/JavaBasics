cf7da6a1af9c6817a0568161129fdb1bc123578f
artem_filkov <filkof@mail.ru>
2022-03-03 20:42:52 +0300

    13.8 задание 2 тесты прошел

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,7 +1,11 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import com.skillbox.airport.Terminal;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +14,23 @@ public class Main {
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        List<Flight> nearestFlights;
+        List<Flight> flights = new ArrayList<>();
+        List<Terminal> terminals = airport.getTerminals();
+
+        terminals.forEach(terminal -> flights.addAll(terminal.getFlights()));
+
+        Calendar c = Calendar.getInstance();
+        Calendar c1 = Calendar.getInstance();
+        c1.add(Calendar.HOUR,2);
+
+        nearestFlights = flights.stream()
+                .filter(flight -> flight.getType() == Flight.Type.DEPARTURE)
+                .filter(flight -> flight.getDate().after(c.getTime()))
+                .filter(flight -> flight.getDate().before(c1.getTime()))
+                .collect(Collectors.toList());
+
+        return nearestFlights;
     }
 
 }
