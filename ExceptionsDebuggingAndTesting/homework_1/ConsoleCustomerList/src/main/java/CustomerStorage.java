da83f1e99f0130cc561775fa874ee43e905f54c0
artem_filkov <filkof@mail.ru>
2022-03-04 16:38:48 +0300

    14.4 passed the tests

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -15,6 +15,23 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
+        String phone = "([+]?[7-8]?)[0-9]{9,10}";
+        String email = "[^\\s]+@[A-Za-z]+\\.[A-Za-z]{2,3}";
+
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong command! Available command examples: \n" +
+                    "add Василий Петров " +
+                    "vasily.petrov@gmail.com +79215637722" );
+        }
+        else if (!components[INDEX_EMAIL].matches(email)){
+            throw new IllegalArgumentException("Wrong email format! Available email format examples: \n" +
+                    "vasily.petrov@gmail.com" );
+        }
+        else if (!components[INDEX_PHONE].matches(phone)){
+            throw new IllegalArgumentException("Wrong phone format! Available phone format examples: \n" +
+                    "+79215637722" );
+        }
+
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
