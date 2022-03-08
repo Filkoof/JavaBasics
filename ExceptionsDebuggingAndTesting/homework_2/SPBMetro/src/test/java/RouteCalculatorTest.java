a1840dd043f98dfa8c209d46ffd614f5c761ea82
artem_filkov <filkof@mail.ru>
2022-03-08 14:51:00 +0300

    14.8 процесс

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class RouteCalculatorTest extends TestCase {
     StationIndex stationIndex = new StationIndex();
-    List<Station> route;
+    RouteCalculator routeCalculator = new RouteCalculator(stationIndex);
 
     @Override
     protected void setUp() throws Exception {
@@ -15,20 +15,21 @@ public class RouteCalculatorTest extends TestCase {
         Line line2 = new Line(2, "Черная");
         Line line3 = new Line (3, "Белая");
 
+        stationIndex.getLine(1).addStation(new Station("Петровская", line1));
+        stationIndex.getLine(1).addStation(new Station("Казанская", line1));
+        stationIndex.getLine(1).addStation(new Station("Летовская", line1));
+
+        stationIndex.getLine(2).addStation(new Station("Морковная", line2));
+        stationIndex.getLine(2).addStation(new Station("Яблочная", line2));
+        stationIndex.getLine(2).addStation(new Station("Кропоткинская", line2));
+
+        stationIndex.getLine(3).addStation(new Station("Сахарная", line3));
+        stationIndex.getLine(3).addStation(new Station("Бумажная", line3));
+
         stationIndex.addLine(line1);
         stationIndex.addLine(line2);
         stationIndex.addLine(line3);
 
-        stationIndex.addStation(new Station("Петровская",line1));
-        stationIndex.addStation(new Station("Казанская",line1));
-        stationIndex.addStation(new Station("Летовская",line1));
-
-        stationIndex.addStation(new Station("Морковная", line2));
-        stationIndex.addStation(new Station("Яблочная", line2));
-        stationIndex.addStation(new Station("Кропоткинская", line2));
-
-        stationIndex.addStation(new Station("Сахарная", line3));
-        stationIndex.addStation(new Station("Бумажная", line3));
 
         List<Station> connection1 = new ArrayList<>();
         connection1.add(stationIndex.getStation("Казанская", 1));
@@ -42,14 +43,25 @@ public class RouteCalculatorTest extends TestCase {
     }
 
     public void testGetShortestRoute(){
+        List<Station> excepted = new ArrayList<>();
+
+        excepted.add(stationIndex.getStation("Петровская", 1));
+        excepted.add(stationIndex.getStation("Казанская", 1));
+        excepted.add(stationIndex.getStation("Летовская", 1));
+
+        List<Station> actual;
+        actual =
+
+        assertEquals(excepted, actual);
     }
 
     public void testCalculateDuration(){
-        route = new ArrayList<>();
+        List<Station> route = new ArrayList<>();
 
         route.add(stationIndex.getStation("Летовская"));
         route.add(stationIndex.getStation("Казанская"));
         route.add(stationIndex.getStation("Морковная"));
+
         double actual = RouteCalculator.calculateDuration(route);
         double excepted = 6;
         assertEquals(excepted, actual);
