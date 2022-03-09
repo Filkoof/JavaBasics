694811757ef9416cb000f242ab4d433668144fbf
artem_filkov <filkof@mail.ru>
2022-03-10 00:20:14 +0300

    14.11 ошибка в pom

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/Main.java
@@ -34,6 +34,7 @@ public class Main {
         }
     }
 
+
     private static RouteCalculator getRouteCalculator() {
         createStationIndex();
         return new RouteCalculator(stationIndex);
