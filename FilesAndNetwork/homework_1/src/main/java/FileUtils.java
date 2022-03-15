bee47985fe588325bf4f98faf81153635f10d20b
artem_filkov <filkof@mail.ru>
2022-03-15 21:31:12 +0300

    15.5 тесты прошел

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,34 @@
+import java.io.File;
+
 public class FileUtils {
 
-    public static long calculateFolderSize(String path) {
-        return 0;
+    public static long calculateFolderSize(String folder) {
+        File file = new File(folder);
+
+        if(file.isFile()){
+            return file.length();
+        }
+
+        long sum = 0;
+        File[] files = file.listFiles();
+        for (File file1 : files){
+            sum += calculateFolderSize(file1.getAbsolutePath());
+        }
+
+        return sum;
+    }
+
+    public static String getReadableSize(long size){
+        int kilobyte = 1024;
+        String[] sizeMultipliers = {"B","Kb","Mb","Gb","Tb"};
+
+        for(int i = 0; i < sizeMultipliers.length; i++){
+            double value = size / Math.pow(kilobyte, i);
+            if (value < kilobyte){
+                return Math.round(value) + sizeMultipliers[i];
+            }
+        }
+
+        return "";
     }
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.util.Scanner;
 
-    }
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            for (; ; ) {
+                System.out.println("Укажите путь: ");
+                Scanner scanner = new Scanner(System.in);
+                String input = scanner.nextLine();
+                System.out.println(FileUtils.getReadableSize(FileUtils.calculateFolderSize(input)));
+            }
+        } catch (Exception ex){
+            System.out.println("Ошибка");
+        }
+     }
 }
