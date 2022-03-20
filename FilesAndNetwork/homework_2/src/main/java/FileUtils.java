508233c9678c8bc8377ade81e0134257d0632bc9
artem_filkov <filkof@mail.ru>
2022-03-20 21:06:43 +0300

    15.7 init

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,26 @@
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        File source = new File(sourceDirectory);
+
+        File[] files = source.listFiles();
+        for(File f : files) {
+            if (f.isFile()){
+                File destination = new File(destinationDirectory);
+                try {
+                    Files.copy(source, destination);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                File destination = new File(destinationDirectory + f.getName());
+                destination.mkdir();
+            }
+        }
     }
 }
+
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (; ; ) {
+            System.out.println("Укажите копируемую папку: ");
+            String sourceDirectory = scanner.nextLine();
+
+            System.out.println("Укажите папку назначения: ");
+            String destinationDirectory = scanner.nextLine();
+
+            FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+        }
+
     }
+
 }
+
