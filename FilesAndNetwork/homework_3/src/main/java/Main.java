44fe45a659f890821e0547252e071b8012009d0a
artem_filkov <filkof@mail.ru>
2022-03-23 22:21:33 +0300

    15.9 init

--- FilesAndNetwork/homework_3/src/main/java/Main.java
+++ FilesAndNetwork/homework_3/src/main/java/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+
+    public static final String MOVEMENT_LIST_CSV = "C:/Users/Professional/desktop/movementList.csv";
+
     public static void main(String[] args) {
     }
 }
--- FilesAndNetwork/homework_3/src/main/java/Movements.java
+++ FilesAndNetwork/homework_3/src/main/java/Movements.java
@@ -1,13 +1,77 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Movements {
 
+    private List<String> operationDescription = new ArrayList<>();
+    private double incomeSum;
+    private double expenseSum;
+
     public Movements(String pathMovementsCsv) {
+        readMovementsFromFile(pathMovementsCsv);
+    }
+
+    public void readMovementsFromFile(String path){
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(path));
+
+            lines.remove(0);
+            for (String line : lines){
+                String[] fragments = oneForm(line).split(",");
+
+                if (fragments.length != 8) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+
+                double income = Double.parseDouble(fragments[6]);
+                double expense = Double.parseDouble(fragments[7]);
+
+                if (income != 0){
+                    incomeSum += income;
+                } else {
+                    expenseSum += expense;
+                }
+
+                if(expense != 0){
+                    operationDescription.add(fragments[5].concat(fragments[7]));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String oneForm(String line){
+        String regex = "\".+\"";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(line);
+
+        while (matcher.find()){
+            int start = matcher.start();
+            int end = matcher.end();
+            line.substring(start, end)
+                    .replaceAll("\"", "")
+                    .replace(",", ".");
+
+            return line;
+
+        }
+        return line;
+
     }
 
     public double getExpenseSum() {
-        return 0.0;
+        return expenseSum;
     }
 
     public double getIncomeSum() {
-        return 0.0;
+        return incomeSum;
     }
 }
