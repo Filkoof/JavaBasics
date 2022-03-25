6f7038109f16b1ab24c5c52c89c7eda2e536519c
artem_filkov <filkof@mail.ru>
2022-03-25 14:05:20 +0300

    15.9 тест с float остался

--- FilesAndNetwork/homework_3/src/main/java/Movements.java
+++ FilesAndNetwork/homework_3/src/main/java/Movements.java
@@ -2,43 +2,47 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Movements {
 
     private static List<String> operationDescription = new ArrayList<>();
-    private static double incomeSum;
-    private static double expenseSum;
+    private double incomeSum;
+    private double expenseSum;
 
     public Movements(String pathMovementsCsv) {
         readMovementsFromFile(pathMovementsCsv);
     }
 
-    public static void readMovementsFromFile(String path){
+    public void readMovementsFromFile(String path) {
         try {
             List<String> lines = Files.readAllLines(Paths.get(path));
 
-
             lines.remove(0);
-            for (String line : lines){
-                String[] fragments = oneForm(line).split(",");
-                double income = Double.parseDouble(fragments[6]);
-                double expense = Double.parseDouble(fragments[7]);
+            for (String line : lines) {
+                String[] fragments = line.split(",", 8);
 
-                if (fragments.length != 8) {
-                    System.out.println("Wrong line: " + line);
-                    continue;
-                }
+                double income = Double.parseDouble(fragments[6].replaceAll("\"", "")
+                        .replace(",", "."));
+                double expense = Double.parseDouble(fragments[7].replaceAll("\"", "")
+                        .replace(",", "."));
+
+//                if (fragments.length != 8) {
+//                    System.out.println("Wrong line: " + line);
+//                    continue;
+//                }
 
                 if (income != 0){
-                    incomeSum += income;
+                    this.incomeSum += income;
                 } else {
-                    expenseSum += expense;
+                    this.expenseSum += expense;
                 }
 
-                if(expense != 0){
+                if (expense != 0) {
                     operationDescription.add(fragments[5].concat(fragments[7]));
                 }
             }
@@ -47,20 +51,19 @@ public class Movements {
         }
     }
 
-    public static String oneForm(String line){
+    public static String oneForm(String line) {
         String regex = "\".+\"";
         String temp = line;
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(temp);
 
-        if (matcher.find()){
+        if (matcher.find()) {
             int start = matcher.start();
             int end = matcher.end();
-            line.substring(start, end + 1)
+            line.substring(start, end)
                     .replaceAll("\"", "")
                     .replace(",", ".");
-
             return temp;
         } else {
             return line;
@@ -74,9 +77,5 @@ public class Movements {
     public double getIncomeSum() {
         return incomeSum;
     }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 }
+
