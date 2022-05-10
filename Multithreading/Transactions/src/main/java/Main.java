de8a55614b148c7542055f81ff5bb9d8be75b4fe
Артем <filkof@mail.ru>
2022-05-10 21:59:42 +0300

    17.13 вывод в мейне

--- Multithreading/Transactions/src/main/java/Main.java
+++ Multithreading/Transactions/src/main/java/Main.java
@@ -8,15 +8,18 @@ public class Main {
             bank.fillAccounts(String.valueOf(i), new Account(String.valueOf(i), balance));
         }
 
+        System.out.println(bank.getSumAllAccounts());
         for (int i = 0; i < coreCount; i++) {
             new Thread(()-> {
-                for (int j = 0; j <= 10_000; j++) {
+                for (int j = 0; j <= 2500; j++) {
                     try {
                         bank.transfer(String.valueOf(1 + (int) (Math.random() * 1000)),
                                 String.valueOf(1 + (int) (Math.random() * 1000)),
                                 10 + (long) (Math.random() * 55_000));
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
+                    } finally {
+                        System.out.println(bank.getSumAllAccounts());
                     }
                 }
             }).start();
